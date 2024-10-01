f22d38763e3ebe4f5e9d1ba8d0a10d3aeaa751e5
Heri Setiawan <36474174+herisetiawan00@users.noreply.github.com>
2024-10-01 13:05:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,13 @@ echo "Shell found: $user_shell"
 
 shell=""
 
-if [ "$user_shell" == "bash" ] && [ -f "$HOME/.bashrc" ]; then
+if [["$user_shell" == "bash" ]] && [[ -f "$HOME/.bashrc" ]]; then
   shell="$HOME/.bashrc"
-elif [ "$user_shell" == "zsh" ] && [ -f "$HOME/.zshrc"]; then
+elif [[ "$user_shell" == "zsh" ]] && [[ -f "$HOME/.zshrc" ]]; then
   shell="$HOME/.zshrc"
 fi
 
-echo "source ~/.dotfile/.jarvis" >>"$shell"
+echo "source ~/.dotfile/.jarvis" >> "$shell"
 . "$shell"
 
 echo "Install complete!"
