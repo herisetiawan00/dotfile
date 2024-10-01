c0f502b7cebadd0ac4f93195b23d302dddb52a75
Heri Setiawan <36474174+herisetiawan00@users.noreply.github.com>
2024-10-01 13:17:13 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Detecting shell..."
 
 user_shell=$(basename "$SHELL")
@@ -10,6 +12,9 @@ if [[ "$user_shell" == "bash" ]] && [[ -f "$HOME/.bashrc" ]]; then
   shell="$HOME/.bashrc"
 elif [[ "$user_shell" == "zsh" ]] && [[ -f "$HOME/.zshrc" ]]; then
   shell="$HOME/.zshrc"
+else
+  echo "Unsupported shell or configuration file not found."
+  exit 1
 fi
 
 echo "Removing from shell"
@@ -18,12 +23,19 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i "" "/source ~\/.dotfile\/.jarvis/d" "$shell"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sed -i "/source ~\/.dotfile\/.jarvis/d" "$shell"
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
 fi
 
 echo "Removing repository..."
 
-rm -r "$HOME/.dotfile"
+rm -rf "$HOME/.dotfile"
 
-source "$shell"
+if [[ -n "$shell" ]]; then
+    source "$shell"
+else
+    echo "No shell configuration file found to source."
+fi
 
 echo "Uninstall complete!"
