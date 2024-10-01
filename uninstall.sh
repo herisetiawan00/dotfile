99a27a333e9de9c39920373ac6867b052dfec7cd
Heri Setiawan <36474174+herisetiawan00@users.noreply.github.com>
2024-10-01 13:08:10 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,18 +6,20 @@ echo "Shell found: $user_shell"
 
 shell=""
 
-if [ "$user_shell" == "bash" ] && [ -f "$HOME/.bashrc" ]; then
+if [[ "$user_shell" == "bash" ]] && [[ -f "$HOME/.bashrc" ]]; then
   shell="$HOME/.bashrc"
-elif [ "$user_shell" == "zsh" ] && [ -f "$HOME/.zshrc"]; then
+elif [[ "$user_shell" == "zsh" ]] && [[ -f "$HOME/.zshrc"]]; then
   shell="$HOME/.zshrc"
 fi
 
 echo "Removing from shell"
 
-sed -i '/source ~\/.dotfile\/.jarvis/d' $shell
+sed -i "/source ~\/.dotfile\/.jarvis/d" "$shell"
 
 echo "Removing repository..."
 
 rm -r "$HOME/.dotfile"
+
+source "$shell"
 
 echo "Uninstall complete!"
