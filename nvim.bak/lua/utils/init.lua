e70775da9d753a26a80e36d39badcfab22fa2dfd
Heri Setiawan <hr.bltz1@gmail.com>
2024-10-20 22:20:51 +0700

    chore: remove bak file

--- nvim.bak/lua/utils/init.lua
+++
@@ -1,9 +0,0 @@
-local bufremove = require("utils.bufremove")
-local format = require("utils.format")
-
---- @class Utils
-local M = {
-  bufremove,
-  format,
-}
-return M
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,31 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({})
+		lspconfig.lua_ls.setup({
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+						return
+					end
+				end
+
+				client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+					runtime = {
+						version = 'LuaJIT'
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME
+						}
+					}
+				})
+			end,
+			settings = {
+				Lua = {}
+			}
+		})
 		lspconfig.dartls.setup({})
 	end,
 }
