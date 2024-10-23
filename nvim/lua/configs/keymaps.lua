1942c9dd85758d445e0054997adc77c8b0ae5840
Heri Setiawan <hr.bltz1@gmail.com>
2024-10-23 09:51:29 +0700

    chore: update keymaps for fzf

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -3,3 +3,7 @@ local map = vim.keymap.set
 map("n", "<C-e>", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neotree" })
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+
+map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find Files" })
+map("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Find in Files" })
+map("n", "<leader>fc", "<cmd>FzfLua lsp_code_actions<cr>", { desc = "Code Actions" })
